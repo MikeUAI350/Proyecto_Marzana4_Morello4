14933104ce9228a25850b36d20a26d1a1577d6c7
MikeUAI350 <MikeAgustin.Marzana@alumnos.uai.edu.ar>
2026-06-12 19:18:15 -0300

    nana

--- Proyecto/AMARENT/UI_Menu_44MM.Designer.cs
+++ Proyecto/AMARENT/UI_Menu_44MM.Designer.cs
@@ -193,16 +193,6 @@
             this.ayudaToolStripMenuItem.Text = "Ayuda";
             this.ayudaToolStripMenuItem.Visible = false;
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(713, 415);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
             // UI_Menu_44MM
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
